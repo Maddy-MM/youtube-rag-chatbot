837d2fdf4f8d82476cf60c7a4ba4cd2e7cf9036c
Madhav Makwana <madhavmakwana.mm@gmail.com>
2026-04-22 14:26:35 +0530

    Update API URL to deployed backend

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import re
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://youtube-rag-backend-js1w.onrender.com" 
 
 st.set_page_config(page_title="YouTube RAG Chatbot")
 
