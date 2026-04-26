4012b59fc8532ee005d4412b708bf0a7584dc7f7
Madhav Makwana <madhavmakwana.mm@gmail.com>
2026-04-26 13:53:12 +0530

    Update API URL

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import requests
 
-# API_URL = "https://youtube-rag-backend-js1w.onrender.com"
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://youtube-rag-backend-js1w.onrender.com"
+# API_URL = "http://127.0.0.1:8000"
 
 st.set_page_config(page_title="YTLens")
 
